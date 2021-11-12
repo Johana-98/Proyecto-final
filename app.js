31268757607032266fe9d57326d75f8230c8fc9d
Johana Quiroga <90170635+Johana-98@users.noreply.github.com>
2021-11-12 11:43:09 -0300

    1er commit
    Funcionalidades de la pagina

---
+++ app.js
@@ -0,0 +1,11 @@
+
+/////////      FUNCION PARA DAR LA DIRECCION ONCLICK BOTON    ////////////
+function darDireccion(direccion) {
+	alert(direccion);
+};
+
+
+/////////      FUNCION PARA HACER PEDIDO ONCLICK BOTON     ////////////
+function hacerPedido() {
+	window.prompt("Describa su Pedido")	
+};
